c71e47c74d0cb3004c8184fb812ca0858882486f
Trevor McDonald <trevor.mcdonald@mrjaffesclass.com>
2023-03-23 13:09:48 -0700

    is winner added

---
+++ Tictactoe2.28/src/main/java/com/mycompany/tictactoe/Tictactoe.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tictactoe;
+
+/**
+ *
+ * @author student
+ */
+public class Tictactoe {
+
+    public static void main(String[] args) {
+        Tictactoe ttt = new Tictactoe();
+       
+    }
+    
+    
+    public Tictactoe() {
+    View view = new View();
+    view.setVisible(true);
+ System.out.print("test");
+    }
+}
